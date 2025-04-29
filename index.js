b05994bb75f0e7db9fae7f4c72688f9f07072ed6
vvoavo <vova1855btf@gmail.com>
2025-04-29 23:33:56 +0300

    У файлі index.js реалізовано надсилання файлу index.ejs з HTML-кодом у відповідь при відвідуванні URL-адреси root(/)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //Підключаємо залежність express
-var express = require('express');
+var express = require("express");
 //Викликаємо express
 var app = express();
-//Налаштовуємо маршрут root(/) URL-адреси
-app.get('/', function (req, res) {
-//При відвідуванні root(/) URL-адреси у відповідь 
-//отримаємо повідомлення "Привіт, мій друже!"
- res.send('Привіт, мій друже!');
+//Налаштовуємо обробник шаблонів
+app.set("view engine", "ejs");
+//Відображаємо index.ejs
+app.get("/", function (req, res) {
+    res.render("index");
 });
 //Налаштовуємо сервер для прослуховування порту 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Сервер працює на порту 3000!");
 });
