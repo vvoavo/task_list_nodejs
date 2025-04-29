f659bb301393d2d86357de508a00f81525663e07
vvoavo <vova1855btf@gmail.com>
2025-04-29 23:50:53 +0300

    У файлі index.js реалізовано крім надсилання файлу index.ejs з HTML-кодом та списку доданих завдань ще списку виконаних завдань у відповідь при відвідуванні URL-адреси root(/)

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 //Визначаємо початковий масив доданих завдань
 var task = ["Опанувати JavaScript", "Опанувати Node.js"];
+//Визначаємо початковий масив виконаних завдань
+var complete = ["Опанувати HTML та CSS", "Опанувати Git та GitHub"];
 //Визначаємо маршрут публікації для додавання нового завдання
 //в масив доданих завдань
 app.post("/addnewtask", function (req, res) {
@@ -19,9 +21,29 @@ app.post("/addnewtask", function (req, res) {
     //Повертаємося до маршруту root(/)
     res.redirect("/");
 });
-//Відображаємо index.ejs і додані завдання
+//Визначаємо маршрут публікації для переміщення нового відміченого як виконане
+//завдання у масив виконаних завдань
+app.post("/movetocompletetask", function (req, res) {
+    var completeTask = req.body.check;
+    //Перевіряємо наявність нового відміченого як виконане завдання і
+    //додаємо його до масиву виконаних завдань
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        //Перевіряємо, чи нове відмічене як виконане завдання вже виходить у
+        //масив виконаних завдань і вилучаємо його з масиву доданих завдань
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (var i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    //Повертаємося до маршруту root(/)
+    res.redirect("/");
+});
+//Відображаємо index.ejs, додані і виконані завдання
 app.get("/", function (req, res) {
-    res.render("index", { task: task });
+    res.render("index", { task: task, complete: complete });
 });
 //Налаштовуємо сервер для прослуховування порту 3000
 app.listen(3000, function () {
