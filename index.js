f9aa2f9cd0e5d8e4135b3bbfd4155fff0d427c72
vvoavo <vova1855btf@gmail.com>
2025-04-29 23:36:41 +0300

    У файлі index.js реалізовано крім надсилання файлу index.ejs з HTML-кодом ще списку доданих завдань у відповідь при відвідуванні URL-адреси root(/)

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 //Підключаємо залежність express
 var express = require("express");
+//Підключаємо залежність body-parser
+var bodyParser = require("body-parser");
 //Викликаємо express
 var app = express();
+//Вмикаємо body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 //Налаштовуємо обробник шаблонів
 app.set("view engine", "ejs");
-//Відображаємо index.ejs
+//Визначаємо початковий масив доданих завдань
+var task = ["Опанувати JavaScript", "Опанувати Node.js"];
+//Визначаємо маршрут публікації для додавання нового завдання
+//в масив доданих завдань
+app.post("/addnewtask", function (req, res) {
+    var newTask = req.body.newtask;
+    //Додаємо нове завдання з маршруту публікації в масив доданих завдань
+    task.push(newTask);
+    //Повертаємося до маршруту root(/)
+    res.redirect("/");
+});
+//Відображаємо index.ejs і додані завдання
 app.get("/", function (req, res) {
-    res.render("index");
+    res.render("index", { task: task });
 });
 //Налаштовуємо сервер для прослуховування порту 3000
 app.listen(3000, function () {
     console.log("Сервер працює на порту 3000!");
 });
-app.post('/addnewtask', function (req, res) {
-    res.render('index')
-  });
-  
